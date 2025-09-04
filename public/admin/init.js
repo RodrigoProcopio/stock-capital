ff139744fe48aa5bb3b12c1b2c2668bdd4b17d44
RodrigoProcopio <digoprocopio@gmail.com>
2025-09-04 10:07:51 -0300

    Ajustes Menu

--- public/admin/init.js
+++ public/admin/init.js
@@ -1,18 +1,16 @@
-// Estilo do preview
+// public/admin/init.js
 CMS.registerPreviewStyle("/index.css");
 document.title = "Stock Capital — CMS";
 
 if (window.netlifyIdentity) {
   window.netlifyIdentity.init();
 
-  // Debug: token na URL
   if (/#(?:invite_token|confirmation_token|recovery_token)=/.test(window.location.hash)) {
     console.log("Token detectado:", window.location.hash);
   }
 
   window.netlifyIdentity.on("login", (user) => {
     console.log("Usuário logado no CMS:", user);
-
     if (/#(?:invite_token|confirmation_token|recovery_token)=/.test(window.location.hash)) {
       window.location.replace("/admin/");
     }
